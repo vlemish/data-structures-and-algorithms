96e3939924e615ec150ed60ffabc6bf67ea7d5b5
MdmaSteel <vlad.lemish123@gmail.com>
2020-08-01 22:14:13 +0300

    Initial

---
+++ QueueList/Queue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue<T> where T : IComparable
+    {
+        private List<T> queues;
+
+        public Queue()
+        {
+            queues = new List<T>();
+        }
+
+        public T Dequeue()
+        {
+            try
+            {
+                T toRemove = queues.First();
+                queues.Remove(toRemove);
+                return toRemove;
+            }
+            catch (Exception) { Console.WriteLine("Unable to delete element from Queue! There is no elements in the query!"); }
+
+            return default(T);
+        }
+
+        public void Enqueue(T data)
+        {
+            queues.Add(data);
+        }
+
+        public T Peek()
+        {
+            return queues.Count==0 ? default(T) : queues.First();
+        }
+    }
+}
